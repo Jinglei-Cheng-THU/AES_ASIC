b6545f8abac99345fcd2d451e3ad63753fd4221d
ThorinLee <ThorinLee@outlook.com>
2018-01-17 01:19:08 +0800

    Encrypt_Core Finished
    Fixed bugs and test with single key and multiple plaintext.

--- Encrypt_Core.v
+++ Encrypt_Core.v
@@ -18,16 +18,15 @@ module Encrypt_Core (
   wire [127:0]key_in;
   wire [127:0]add_out;
 
-  wire [127:0]shift_in;
-  wire [127:0]pre_shift[0:3];
-  wire [127:0]shift_out[0:3];
-  wire [127:0]tbox_out[0:3];
-  wire [127:0]tbox_out_r[0:3];
+  reg [127:0]ss_in;
+  wire [127:0]ss_out;
 
-
+  reg [127:0]mix_in;
   wire [127:0]mix_out;
 
+
   reg [127:0]round_result;
+  reg [127:0]round_result_w;
   reg [3:0]rounds;
   reg operation;
   reg final_round;
@@ -39,8 +38,8 @@ module Encrypt_Core (
       c_ready <= 0;
     end
     else begin
-      if(Addr == rounds) begin
-        Ciphertext <= round_result ^ key_in;
+      if((operation && (Addr == rounds))|(~operation && (Addr == 4'h0))) begin
+        Ciphertext <= add_out;
         c_ready <= 1;
       end
       else begin
@@ -64,13 +63,12 @@ module Encrypt_Core (
           round_result <= Plain_text;
           Core_Busy <= 1;
           operation <= op;
+          rounds <= Nr;
           if(op) begin
             Addr <= 4'h0;
-            rounds <= Nr;
           end
           else begin
             Addr <= Nr;
-            rounds <= 4'h0;
           end
         end
         else begin
@@ -85,9 +83,9 @@ module Encrypt_Core (
         operation <= operation;
         rounds <= rounds;
         if (Key[128]) begin
-          if(Addr == rounds) begin
+          if((operation && (Addr == rounds))|(~operation && (Addr == 4'h0))) begin
             Core_Busy <= 0;
-            Addr <= 4'h0;
+            Addr <= 4'h1;
           end
           else begin
             if(operation) begin
@@ -98,7 +96,7 @@ module Encrypt_Core (
             end
             Core_Busy <= Core_Busy;
           end
-          round_result <= mix_out;
+          round_result <= round_result_w;
         end
         else begin
           round_result <= round_result;
@@ -109,47 +107,118 @@ module Encrypt_Core (
     end
   end
 
-  //add round key
-  assign add_in = round_result;
-  assign add_out = add_in ^ key_in;
-  //tbox and shift row
-  assign shift_in = add_out;
+  always @ ( * ) begin
+    if(operation) begin
+      if(Addr == rounds - 1) begin
+        round_result_w = ss_out;
+      end
+      else begin
+        round_result_w = mix_out;
+      end
+      mix_in = ss_out;
+      ss_in = add_out;
+    end
+    else begin
+      round_result_w = ss_out;
+      mix_in = add_out;
+      if(Addr == rounds) begin
+        ss_in = add_out;
+      end
+      else begin
+        ss_in = mix_out;
+      end
+    end
+  end
 
+  assign add_in = round_result;
+  //add round key
+  add adder(add_in,key_in,add_out);
+
+  //tbox and shift row
+  shift_and_sub ss(ss_in,operation,ss_out);
+
+  //mix column
+  mix_col mc(mix_in,operation,mix_out);
+
+endmodule
+
+module shift_and_sub(
+  input [127:0]ss_in,
+  input op,
+  output [127:0]ss_out
+  );
+  wire [127:0]pre_shift;
+  wire [127:0]sbox_out;
+  wire [127:0]sbox_out_r;
+  assign pre_shift = op ? sbox_out : sbox_out_r;
   generate
     genvar i;
-    for (i = 0; i < 4; i = i + 1) begin:assign_tbox_out
-      assign pre_shift[i] = op ? tbox_out[i] : tbox_out_r[i];
-    end
     for (i = 0; i < 16; i = i + 1) begin:gen_tbox
-      aes_tbox tboxs(shift_in[8*i+7:8*i],{tbox_out[3][8*i+7:8*i],tbox_out[2][8*i+7:8*i],tbox_out[1][8*i+7:8*i],tbox_out[0][8*i+7:8*i]});
-      aes_tbox_r tboxs_r(shift_in[8*i+7:8*i],{tbox_out_r[3][8*i+7:8*i],tbox_out_r[2][8*i+7:8*i],tbox_out_r[1][8*i+7:8*i],tbox_out_r[0][8*i+7:8*i]});
+      aes_sbox sboxs(ss_in[8*i+7:8*i],sbox_out[8*i+7:8*i]);
+      aes_sbox_r sboxs_r(ss_in[8*i+7:8*i],sbox_out_r[8*i+7:8*i]);
     end
-    genvar c,r,t;
+    genvar c,r;
     for (c = 0; c < 4; c = c + 1) begin:select_column
       for (r = 0; r < 4; r = r + 1) begin:select_row
-        for (t = 0; t < 4; t= t + 1) begin:select_times
-          assign shift_out[t][32*c+8*r+7:32*c+8*r] =
-              operation ? pre_shift[t][((4*c+4*r+r+16)%16)*8+7:((4*c+4*r+r+16)%16)*8] :
-                          pre_shift[t][((4*c-4*r+r+16)%16)*8+7:((4*c-4*r+r+16)%16)*8];
-        end
+        assign ss_out[32*c+8*r+7:32*c+8*r] = op ?
+            pre_shift[((4*c+4*r+r+16)%16)*8+7:((4*c+4*r+r+16)%16)*8] :
+            pre_shift[((4*c-4*r+r+16)%16)*8+7:((4*c-4*r+r+16)%16)*8];
       end
     end
   endgenerate
+endmodule
 
-  //mix column
+module mix_col(
+  input [127:0]mix_in,
+  input op,
+  output [127:0]mix_out
+  );
+  wire [127:0]mix_in_2;
+  wire [127:0]mix_in_4;
+  wire [127:0]mix_in_8;
+  wire [127:0]mix_in_3;
+  wire [127:0]mix_in_9;
+  wire [127:0]mix_in_b;
+  wire [127:0]mix_in_d;
+  wire [127:0]mix_in_e;
   generate
-    genvar m;
-    for (m = 0; m < 4; m = m + 1) begin : gen_mix
-      assign mix_out[m*32+31:m*32] = ((operation & (Addr == (rounds - 1)))|(~operation & (Addr == (rounds + 1)))) ? operation ? shift_out[0][m*32+31:m*32] :
-        { shift_out[2][m*32+31:m*32+24]^shift_out[1][m*32+31:m*32+24]^shift_out[0][m*32+31:m*32+24]^shift_out[3][m*32+31:m*32+24],
-          shift_out[1][m*32+23:m*32+16]^shift_out[0][m*32+23:m*32+16]^shift_out[3][m*32+23:m*32+16]^shift_out[2][m*32+23:m*32+16],
-          shift_out[0][m*32+15:m*32+8]^shift_out[3][m*32+15:m*32+8]^shift_out[2][m*32+15:m*32+8]^shift_out[1][m*32+15:m*32+8],
-          shift_out[3][m*32+7:m*32]^shift_out[2][m*32+7:m*32]^shift_out[1][m*32+7:m*32]^shift_out[0][m*32+7:m*32]} :
-        { shift_out[2][m*32+7:m*32]^shift_out[1][m*32+15:m*32+8]^shift_out[0][m*32+23:m*32+16]^shift_out[3][m*32+31:m*32+24],
-          shift_out[1][m*32+7:m*32]^shift_out[0][m*32+15:m*32+8]^shift_out[3][m*32+23:m*32+16]^shift_out[2][m*32+31:m*32+24],
-          shift_out[0][m*32+7:m*32]^shift_out[3][m*32+15:m*32+8]^shift_out[2][m*32+23:m*32+16]^shift_out[1][m*32+31:m*32+24],
-          shift_out[3][m*32+7:m*32]^shift_out[2][m*32+15:m*32+8]^shift_out[1][m*32+23:m*32+16]^shift_out[0][m*32+31:m*32+24]};
+    genvar i,m;
+    for (i = 0; i < 16; i = i + 1) begin : generate_mul_2
+      mul_by_2 m2(mix_in[8*i+7:8*i],mix_in_2[8*i+7:8*i]);
+      mul_by_2 m4(mix_in_2[8*i+7:8*i],mix_in_4[8*i+7:8*i]);
+      mul_by_2 m8(mix_in_4[8*i+7:8*i],mix_in_8[8*i+7:8*i]);
+    end
+    for (m = 0; m < 4; m = m + 1) begin : generate_mix_out
+      assign mix_out[m*32+31:m*32] = (op) ?
+        { mix_in_3[m*32+7:m*32]^mix_in[m*32+15:m*32+8]^mix_in[m*32+23:m*32+16]^mix_in_2[m*32+31:m*32+24],
+          mix_in[m*32+7:m*32]^mix_in[m*32+15:m*32+8]^mix_in_2[m*32+23:m*32+16]^mix_in_3[m*32+31:m*32+24],
+          mix_in[m*32+7:m*32]^mix_in_2[m*32+15:m*32+8]^mix_in_3[m*32+23:m*32+16]^mix_in[m*32+31:m*32+24],
+          mix_in_2[m*32+7:m*32]^mix_in_3[m*32+15:m*32+8]^mix_in[m*32+23:m*32+16]^mix_in[m*32+31:m*32+24]} :
+        { mix_in_b[m*32+7:m*32]^mix_in_d[m*32+15:m*32+8]^mix_in_9[m*32+23:m*32+16]^mix_in_e[m*32+31:m*32+24],
+          mix_in_d[m*32+7:m*32]^mix_in_9[m*32+15:m*32+8]^mix_in_e[m*32+23:m*32+16]^mix_in_b[m*32+31:m*32+24],
+          mix_in_9[m*32+7:m*32]^mix_in_e[m*32+15:m*32+8]^mix_in_b[m*32+23:m*32+16]^mix_in_d[m*32+31:m*32+24],
+          mix_in_e[m*32+7:m*32]^mix_in_b[m*32+15:m*32+8]^mix_in_d[m*32+23:m*32+16]^mix_in_9[m*32+31:m*32+24]};
     end
   endgenerate
+  assign mix_in_3 = mix_in_2 ^ mix_in;
+  assign mix_in_9 = mix_in_8 ^ mix_in;
+  assign mix_in_b = mix_in_8 ^ mix_in_2 ^ mix_in;
+  assign mix_in_d = mix_in_8 ^ mix_in_4 ^ mix_in;
+  assign mix_in_e = mix_in_8 ^ mix_in_4 ^ mix_in_2;
 
+endmodule
+
+module mul_by_2 (
+  input [7:0]mul_in,
+  output [7:0]mul_out
+  );
+  assign mul_out = mul_in[7] ? (mul_in << 1)^8'h1b : mul_in << 1;
+endmodule
+
+module add(
+  input [127:0] add_1,
+  input [127:0] add_2,
+  output [127:0] add_res
+  );
+  assign add_res = add_1 ^ add_2;
 endmodule
